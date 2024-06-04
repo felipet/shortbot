fa616e8dee1d2a8a35b84dbe0c80d61a8e7a20c5
Felipe Torres González <torresfelipex1@gmail.com>
2024-06-04 16:38:10 +0200

    Feature to parse data from the CNMV web page regarding the alive short positions of a value (#1)
    * Add handler for the CNMV web page
    
    * Rustfmt applied to cnmv_scrapper.rs
    
    * New deps
    
    * Applied Clippy suggestions
    
    * Helper descriptors for the CNMV provider
    
    * Receivestock endpoint
    
    * Improved waiting message for the receivestock endpoint

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,26 @@
 //! Library of the ShortBot crate.
 
-use teloxide::utils::command::BotCommands;
+use teloxide::{
+    dispatching::dialogue::{Dialogue, InMemStorage},
+    utils::command::BotCommands,
+};
 
 pub mod configuration;
 pub mod telemetry;
 
+/// Name of the data file that contains the descriptors for the Ibex35 companies.
+pub const IBEX35_STOCK_DESCRIPTORS: &str = "ibex35.toml";
+
 // Bring all the endpoints to the main context.
 pub mod endpoints {
     mod help;
+    mod liststocks;
+    mod receivestock;
     mod start;
 
     pub use help::help;
+    pub use liststocks::list_stocks;
+    pub use receivestock::receive_stock;
     pub use start::start;
 }
 
@@ -23,6 +33,8 @@ pub mod handlers {
 
 type HandlerResult = Result<(), Box<dyn std::error::Error + Send + Sync>>;
 
+type ShortBotDialogue = Dialogue<State, InMemStorage<State>>;
+
 /// State machine
 ///
 /// # Description
@@ -32,6 +44,8 @@ type HandlerResult = Result<(), Box<dyn std::error::Error + Send + Sync>>;
 pub enum State {
     #[default]
     Start,
+    ListStocks,
+    ReceiveStock,
 }
 
 /// Application commands in English language
@@ -41,7 +55,7 @@ pub enum State {
 /// TODO! document the commands.
 #[derive(BotCommands, Clone)]
 #[command(
-    rename_rule = "camelCase",
+    rename_rule = "lowercase",
     description = "These commands are supported by the Bot:"
 )]
 pub enum CommandEng {
@@ -49,4 +63,96 @@ pub enum CommandEng {
     Start,
     #[command(description = "Display this message.")]
     Help,
+    #[command(description = "List available stocks.")]
+    ChooseStock,
+}
+
+/// Finance module.
+///
+/// # Description
+///
+/// This module includes all the logic related to extract and process financial data.
+pub mod finance {
+    mod cnmv_scrapper;
+    mod ibex35;
+    mod ibex_company;
+
+    use core::fmt;
+
+    pub use cnmv_scrapper::CNMVProvider;
+    pub use ibex35::{load_ibex35_companies, Ibex35Market};
+    pub use ibex_company::IbexCompany;
+
+    use date::Date;
+
+    /// Short position descriptor.
+    #[derive(Debug)]
+    pub struct ShortPosition {
+        /// This is the name of the investment fund that owns the short position.
+        pub owner: String,
+        /// This is a percentage over the company's total capitalization that indicates
+        /// the amount of shares sold in short by the owner against the value of the
+        /// company.
+        pub weight: f32,
+        /// Date in which the short position was stated.
+        pub date: String,
+    }
+
+    impl fmt::Display for ShortPosition {
+        fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+            write!(f, "{} - {} ({})", self.owner, self.weight, self.date)
+        }
+    }
+
+    /// Container of active short positions of a company.
+    ///
+    /// # Description
+    ///
+    /// This `struct` gathers all the active short positions of a company. It is alike to
+    /// the table shown in the web page when checking for the short positions of a company.
+    ///
+    /// Short positions are stated once per day, no later than 15:30. Thus a full timestamp
+    /// is not really useful. Only the date is kept for the entries.
+    #[derive(Debug)]
+    pub struct AliveShortPositions {
+        /// Summation of all the active [ShortPosition::weight] of the company.
+        pub total: f32,
+        /// Collection of active [ShortPosition] for a company.
+        pub positions: Vec<ShortPosition>,
+        /// Timestamp of the active positions.
+        pub date: Date,
+    }
+
+    impl AliveShortPositions {
+        /// Constructor of the [AliveShortPositions] class.
+        pub fn new() -> AliveShortPositions {
+            AliveShortPositions {
+                total: 0.0,
+                positions: Vec::new(),
+                date: Date::today_utc(),
+            }
+        }
+    }
+
+    impl Default for AliveShortPositions {
+        fn default() -> Self {
+            Self::new()
+        }
+    }
+
+    impl fmt::Display for AliveShortPositions {
+        fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+            for position in self.positions.iter() {
+                writeln!(
+                    f,
+                    "✓ {}: <b>{} %</b> ({})",
+                    position.owner.as_str(),
+                    position.weight,
+                    position.date
+                )?;
+            }
+
+            Ok(())
+        }
+    }
 }
